f660911ca969b0080f21556c4cd60419d9a63f2a
Cristian  Zapata <85204418+CristianZapata03@users.noreply.github.com>
2021-12-11 19:02:24 -0500

    Ingreso Usuario

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+  function ir(){
+
+var c=1017200994;
+var u="cczapatai";
+      if(document.form.Clave.value==c && document.form.usuario.value==u){
+      alet (Bienvenido);
+  window.location=;"index.html";}
+  else{
+  alert("Ingresar Usuario y Contraseña correcta");
+  }
